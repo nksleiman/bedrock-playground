fe5cf703017194ce1419702a35c58d1bb0a98ec6
Noah Sleiman <nksleiman@gmail.com>
2024-01-20 23:39:29 -0800

    implement langchain prompttemplate

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,19 +1,26 @@
-# Import necessary libraries
-import utils
 import streamlit as st
+import utils
+
+
+# Function to handle the main logic
+def process_input(chef_selection, prompt):
+    response = utils.send_prompt_langchain(chef_selection, prompt)
+    st.write(response)
+
 
 # Set up the Streamlit interface
-st.title("Bedrock Portal")
+st.title("Cook With A Famous Chef")
 
-# Create a text area in the sidebar for user input
-prompt = st.sidebar.text_area(label="What is your question?", max_chars=2000)
+# Dropdown for chef selection
+chef_selection = st.sidebar.selectbox(
+    "Select Your Chef",
+    ["", "Anthony Bourdain", "Alton Brown", "Gordon Ramsey"],
+    index=0,
+)
 
-# Check if there is a prompt provided by the user
-if prompt:
-    # Send the prompt to the utils function and get a response
-    # response = utils.send_prompt(prompt)
-    response = utils.send_prompt_langchain(prompt)
-    
-    # Display the response from the support AI in the main page area
-    # st.write("Support AI: ", response["generation"])
-    st.write(response)
+# Text area for user input
+prompt_input = st.sidebar.text_area("What is your question?", max_chars=2000)
+
+# Button to trigger the processing
+if st.sidebar.button("Submit"):
+    process_input(chef_selection, prompt_input)
